14706e8b2b1b7689242d6c808830e9dd6aac5ac2
Sebastian Enerstrand <sebastian@temtron.se>
2026-08-26 22:03:42 +0200

    Added DTOs, endpoint to create books

--- BookQuotes.Api/Controllers/BooksController.cs
+++ BookQuotes.Api/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 ﻿using BookQuotes.Api.Domain;
+using BookQuotes.Api.Features.Books.Contracts;
 using BookQuotes.Api.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,14 +18,54 @@ namespace BookQuotes.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Book>>> GetBooksAsync()
+        public async Task<ActionResult<List<BookResponse>>> GetBooksAsync()
         {
             var books = await _dbContext.Books
                 .AsNoTracking()
                 .OrderBy(b => b.Title)
+                .Select(b => new BookResponse
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    PublicationDate = b.PublicationDate
+                })
                 .ToListAsync();
 
-            return books;
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookResponse>> CreateBookAsync(CreateBookRequest book)
+        {
+            if (book.PublicationDate is null)
+            {
+                return BadRequest("Publication date is required.");
+            }
+
+            var newBook = new Book
+            {
+                Title = book.Title,
+                Author = book.Author,
+                PublicationDate = book.PublicationDate.Value
+            };
+
+            _dbContext.Books.Add(newBook);
+            var result = await _dbContext.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                var bookResponse = new BookResponse
+                {
+                    Id = newBook.Id,
+                    Title = newBook.Title,
+                    Author = newBook.Author,
+                    PublicationDate = newBook.PublicationDate
+                };
+                return Created($"/api/books/{newBook.Id}", bookResponse);
+            }
+
+            return BadRequest();
         }
     }
 }
---
+++ BookQuotes.Api/Features/Books/Contracts/BookResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BookQuotes.Api.Features.Books.Contracts
+{
+    public class BookResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+    }
+}
